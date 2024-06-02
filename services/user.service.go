19d470db1ece7e3e94f33f9d2027c0277e484403
kenzidjulfri <kenzimuhammad@gmail.com>
2024-06-03 00:40:21 +0700

    feat:update registration logic

--- services/user.service.go
+++ services/user.service.go
@@ -1,10 +1,60 @@
 package services
 
 import (
+	"ngobar/berkicau/helper"
+	"ngobar/berkicau/models"
+	"ngobar/berkicau/repositories"
 	"ngobar/berkicau/requests"
 )
 
 type UserService interface {
 	RegisterNewUser(requests.RegistrationRequest) (valid bool, message string)
 	// CheckLoginInformation(requests.RegistrationRequest) (valid bool, message string)
+}
+
+type userService struct {
+	userRepo repositories.UserRepository
+}
+
+func NewUserService() UserService {
+	userRepo := repositories.NewUserRepository(helper.DB)
+	return &userService{userRepo: userRepo}
+}
+
+func (s *userService) RegisterNewUser(request requests.RegistrationRequest) (valid bool, message string) {
+	user, err := s.userRepo.GetDataByEmail(request.Email)
+	if err != nil {
+		return false, err.Error()
+	}
+
+	if &user != nil {
+		return false, "Email is already registered"
+	}
+
+	user, err = s.userRepo.GetDataByUsername(request.Username)
+	if err != nil {
+		return false, err.Error()
+	}
+
+	if &user != nil {
+		return false, "Username is already used"
+	}
+
+	if request.Password != request.PasswordConfirmation {
+		return false, "Password does not match"
+	}
+
+	user = models.User{
+		Email: request.Email,
+		Username: request.Username,
+		FullName: request.FullName,
+		Password: request.Password,
+	}
+
+	_, err = s.userRepo.Create(user)
+	if err != nil {
+		return false, err.Error()
+	}
+
+	return true, "Success"
 }
